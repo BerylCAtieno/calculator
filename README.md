5b16eb6cbd3b42d499bc2a0c88b9c52cd30f7b3d
berylatieno30 <berylatieno30@gmail.com>
2024-11-04 14:38:56 +0000

    display elements on click

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # calculator
 Calculator in HTML, CSS, JavaScript
+
+# TO-DO:
+ - Get elements by id, class
+ - get element by css styling code
+ - reflecting button values to display
--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+// Get Elements
+const display = document.getElementById("display");
+const numberButtons = document.querySelectorAll(".number");
+const operationButtons = document.querySelectorAll(".sign")
+const deleteButton = document.getElementById("delete")
+const equalSign = document.getElementById("equal-sign")
+
+// Display Elements on Click
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      display.value += button.value;
+    });
+  });
+
+  operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      display.value += button.value;
+    });
+  });
+
+
+function handleButtonClick(value) {
+
+
+}
+
+function updateDisplayValue() {
+
+}
+
 // Operations Logic
 
 function add(a, b) {
